0f5060fe6dfbbbc4685aab56028d949c075aacc1
Rodrimon <rodrimonrodriguez@gmail.com>
2023-06-30 10:41:46 -0600

    Camara_cambiada
    Cambie la camara

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector3 CamOffset = new Vector3(0f, 3.66f, -4.91f);
+    public Vector3 Camfirst = new Vector3(0f, 0f, 0f);
+    private Transform _target;
+
     void Start()
     {
-        
+        _target = GameObject.Find("CamPos").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+            this.transform.position = _target.TransformPoint(Camfirst);
+            this.transform.LookAt(_target);
+        }
+        else
+        {
+            this.transform.position = _target.TransformPoint(CamOffset);
+            this.transform.LookAt(_target);
+        }
     }
 }
